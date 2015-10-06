cb2283006f00fd14af0124a39571c6f86667fedc
Shenung <shenung@mail.fresnostate.edu>
2015-10-06 10:21:19 -0700

    presentation 10 exercises/other ex.

---
+++ goUUID/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nu7hatch/gouuid"
+)
+
+func main() {
+	u4, err := uuid.NewV4()
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println(u4)
+}
---
+++ presentation ex/presentation10/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Shenung Fouamvung"
+
+	fmt.Println("name:", name)
+}
