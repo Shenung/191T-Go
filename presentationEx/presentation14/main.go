13af63fe1b0eeb42759d1c30594032c040510780
Shenung <shenung@mail.fresnostate.edu>
2015-11-17 11:41:40 -0800

    finished presentation14 examples

---
+++ presentationEx/presentation14/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+	var x int = 4
+	var y float64 = 10.222
+	fmt.Println(y + float64(x))
+	fmt.Println(int(y))
+
+	fmt.Println(string([]byte{':', 'D'}))
+	fmt.Println([]byte(";)"))
+
+	var max = "This is suppose to be used for the max length"
+	var s = "-42"
+	fmt.Print(strconv.Atoi(s))
+	for i := 0; i < len(max); i++ {
+		fmt.Println("Wait, give me", strconv.Itoa(i), "mins.")
+		fmt.Print("\tlook im escaping\n")
+		fmt.Println("and im a rune from", i, ": ", max[i])
+		fmt.Println("watch me slice this: ", max[:i])
+		fmt.Println("and now to stitch it back up: ", max[:i]+max[i:])
+	}
+
+	var num float64
+	fmt.Println("give me a number to round up: ")
+	fmt.Scanln(&num)
+	fmt.Println("rounded number: ", math.Ceil(num))
+
+	fmt.Println("Find the type of a var at golang.org/pkg/reflect")
+}
