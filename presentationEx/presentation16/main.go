eb5ea76d6ad9f8480c570f3102dc6a9f904048ca
Shenung <shenung@mail.fresnostate.edu>
2015-12-05 23:49:00 -0800

    added more stuff

--- presentationEx/presentation16/main.go
+++ presentationEx/presentation16/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func even(num int) (int, bool) {
+	if num%2 == 0 {
+		return num, true
+	}
+	return num, false
+}
+
+func max(numbers ...int) int {
+	var largest int
+	for _, v := range numbers {
+		if v > largest {
+			largest = v
+		}
+	}
+	return largest
+}
+
+func info(name string, age int) {
+	fmt.Println(name, " is ", age, " years old.")
+}
+
 func main() {
 	// Write a function which takes an integer and returns two values:
 	// the integer divided by 2
@@ -7,14 +30,31 @@ func main() {
 	// For example
 	// 	half(1) should return (0, false)
 	// 	half(2) should return (1, true).
+	var num, truth = even(2)
+	fmt.Println(num, ", ", truth)
+
+	num, truth = even(1)
+	fmt.Println(num, ", ", truth)
 
 	// Write a function with one variadic parameter that finds the greatest number in a list of numbers.
+	greatest := max(4, 7, 9, 123, 543, 23, 435, 53, 125)
+	fmt.Println(greatest)
 
 	// Write a program that prints the value of this expression:
 	//  (true && false) || (false && true) || !(false && false)
+	fmt.Println((true && false) || (false && true) || !(false && false))
 
 	// Write a program that calls a function which takes first name and age
 	// then returns a string like this, “John is 27 years old.”
+	var name string
+	var age int
+	fmt.Print("Name: ")
+	fmt.Scanln(&name)
+	fmt.Println()
+	fmt.Print("age: ")
+	fmt.Scanln(&age)
+	fmt.Println()
+	info(name, age)
 
 	// Write a program that calls a function which takes first name and age
 	// then returns an int and a bool
